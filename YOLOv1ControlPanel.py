e325686592c2e7543f2594e581a00748ccacd87e
slyb2020 <slyb@tju.edu.cn>
2022-02-08 21:41:11 +0800

    2022年2月8日21:40在zenBook上提交,完成YOLOv1面板的图片显示功能,开始实现模型控制面板

--- YOLOv1ControlPanel.py
+++ YOLOv1ControlPanel.py
@@ -1,6 +1,8 @@
 import wx
 from ID_DEFINE import *
 import os
+
+
 class YOLOv1ControlPanel(wx.Panel):
     def __init__(self, parent, log, size):
         wx.Panel.__init__(self, parent, -1, size=size, style=wx.FULL_REPAINT_ON_RESIZE | wx.BORDER_THEME)
@@ -12,17 +14,18 @@ class YOLOv1ControlPanel(wx.Panel):
         vbox.Add((-1, 5))
         hhbox.Add((10, -1))
         hhbox.Add(wx.StaticText(self, label="选择预训练模型："), wx.TOP, 5)
-        self.pretrainedModelCOMBO = wx.ComboBox(self, value=self.pretrainedModelList[0], size=(200, -1), choices=self.pretrainedModelList)
+        self.pretrainedModelCOMBO = wx.ComboBox(self, value=self.pretrainedModelList[0], size=(200, -1),
+                                                choices=self.pretrainedModelList)
         hhbox.Add(self.pretrainedModelCOMBO, 1, wx.RIGHT, 10)
         vbox.Add(hhbox)
-        vbox.Add((-1,5))
+        vbox.Add((-1, 5))
         hhbox = wx.BoxSizer()
-        hhbox.Add((8,-1))
-        self.startDetectionBTN = wx.Button(self, label="开始检测", size=(100,30))
-        hhbox.Add(self.startDetectionBTN,0)
-        hhbox.Add((15,-1))
+        hhbox.Add((8, -1))
+        self.startDetectionBTN = wx.Button(self, label="开始检测", size=(100, 30))
+        hhbox.Add(self.startDetectionBTN, 0)
+        hhbox.Add((15, -1))
         hhbox.Add(wx.StaticText(self, label="检测用时："), wx.TOP, 5)
-        self.spendTimeTXT = wx.TextCtrl(self,size=(100,-1))
-        hhbox.Add(self.spendTimeTXT,0,wx.LEFT,5)
+        self.spendTimeTXT = wx.TextCtrl(self, size=(100, -1))
+        hhbox.Add(self.spendTimeTXT, 0, wx.LEFT, 5)
         vbox.Add(hhbox)
         self.SetSizer(vbox)
