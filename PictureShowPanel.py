03842e0d4d443406b543a74cfea649701b6b3caa
slyb2020 <slyb@tju.edu.cn>
2022-02-10 00:19:13 +0800

    2022年2月10日0:18在T14上提交,实现原图消隐与显示功能

--- PictureShowPanel.py
+++ PictureShowPanel.py
@@ -1,5 +1,6 @@
 import copy
 import cv2
+import torch
 import wx
 from ID_DEFINE import *
 from PIL import Image
@@ -41,6 +42,8 @@ class YOLOPictureShowPanel(wx.Panel):
         self.filename = None
         self.labelList = None
         self.gap = 1 if gap else 0
+        self.bbox = None
+        self.enablePicture = True
 
         gbox = wx.GridSizer(7, 0, 0)
         self.buttonArray = []
@@ -60,6 +63,9 @@ class YOLOPictureShowPanel(wx.Panel):
             self.img = cv2.imread(self.filename)
             self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
             self.width, self.height = self.img.shape[1], self.img.shape[0]
+            shape = self.img.shape
+            if not self.enablePicture:
+                self.img = np.array(torch.zeros(size=shape, dtype=torch.uint8) + 255)
             for label in self.labelList:
                 pt1 = (int(label[1][0] * self.width - label[1][2] * self.width / 2),
                        int(label[1][1] * self.height - label[1][3] * self.height / 2))
@@ -67,6 +73,19 @@ class YOLOPictureShowPanel(wx.Panel):
                        int(label[1][1] * self.height + label[1][3] * self.height / 2))
                 cv2.putText(self.img, CLASSES[int(label[0])], pt1, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
                 cv2.rectangle(self.img, pt1, pt2, (0, 0, 255, 2))
+            if self.bbox != None:
+                h, w = self.img.shape[0:2]
+                n = self.bbox.size()[0]
+                for i in range(n):
+                    p1 = (self.width * self.bbox[i, 1], self.height * self.bbox[i, 2])
+                    p2 = (self.width * self.bbox[i, 3], self.height * self.bbox[i, 4])
+                    cls_name = CLASSES[int(self.bbox[i, 0])]
+                    confidence = self.bbox[i, 5]
+                    # p1 = p1.numpy()
+                    p1 = (int(p1[0].detach().numpy()), int(p1[1].detach().numpy()))
+                    p2 = (400, 400)
+                    cv2.rectangle(self.img, p1, p2, color=COLOR[int(self.bbox[i, 0])])
+                    cv2.putText(self.img, cls_name, p1, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
             x, y = self.img.shape[1] / len(self.buttonArray[0]), self.img.shape[0] / len(self.buttonArray)
             w, h = self.buttonArray[0][0].GetSize()
             for row, rowButtonList in enumerate(self.buttonArray):
--- YOLOv1ControlPanel.py
+++ YOLOv1ControlPanel.py
@@ -21,37 +21,57 @@ from tqdm import tqdm  # python的进度条模块
 import wx.lib.scrolledpanel as scrolled
 from ListDataPanel import ResultDisplayPanel
 
-# class ResultDisplayPanel(scrolled.ScrolledPanel):
-#     def __init__(self, parent, log):
-#         self.log = log
-#         self.parent = parent
-#         scrolled.ScrolledPanel.__init__(self, parent, -1)
-#         self.Recreate()
-#
-#     def Recreate(self):
-#         self.DestroyChildren()
-#         vbox = wx.BoxSizer(wx.VERTICAL)
-#         if self.parent.currentPosition:
-#             row = self.parent.currentPosition[0]
-#             col = self.parent.currentPosition[1]
-#             bbox = []
-#             bbox.append(self.parent.bbox[row*7+col])
-#             bbox.append(self.parent.bbox[2*(row*7+col)])
-#             vbox.Add((-1, 5))
-#             hhbox = wx.BoxSizer()
-#             hhbox.Add((10, -1))
-#             hhbox.Add(wx.StaticText(self, label="第%d行，第%d列BOX数据：" % (row + 1, col + 1)))
-#             vbox.Add(hhbox, 0, wx.EXPAND)
-#             for box in bbox:
-#                 hhbox = wx.BoxSizer()
-#                 hhbox.Add((10, -1))
-#                 hhbox.Add(wx.StaticText(self, label="中心点:", size=(50, -1)), 0, wx.TOP, 5)
-#                 self.centerPointTXT=wx.TextCtrl(self, size=(60, -1))
-#                 hhbox.Add(self.centerPointTXT, 0)
-#                 vbox.Add(hhbox, 0, wx.EXPAND)
-#         self.SetSizer(vbox)
-#         self.SetAutoLayout(1)
-#         self.SetupScrolling()
+
+def draw_bbox(img, bbox):
+    """
+    根据bbox的信息在图像上绘制bounding box
+    :param img: 绘制bbox的图像
+    :param bbox: 是(n,6)的尺寸，其中第0列代表bbox的分类序号，1~4为bbox坐标信息(xyxy)(均归一化了)，5是bbox的专属类别置信度
+    """
+    img = img.copy()
+    h, w = img.shape[0:2]
+    n = bbox.size()[0]
+    for i in range(n):
+        p1 = (w * bbox[i, 1], h * bbox[i, 2])
+        p2 = (w * bbox[i, 3], h * bbox[i, 4])
+        cls_name = CLASSES[int(bbox[i, 0])]
+        confidence = bbox[i, 5]
+        # p1 = p1.numpy()
+        p1 = (int(p1[0].detach().numpy()), int(p1[1].detach().numpy()))
+        p2 = (400, 400)
+        cv2.rectangle(img, p1, p2, color=COLOR[int(bbox[i, 0])])
+        cv2.putText(img, cls_name, p1, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+    # cv2.rectangle(img, (10,10), (100,100), (255,0,0), 2)
+    cv2.imshow("bbox", img)
+    cv2.waitKey(0)
+
+
+def NMS(bbox, conf_thresh=0.1, iou_thresh=0.3):
+    """bbox数据格式是(n,25),前4个是(x1,y1,x2,y2)的坐标信息，第5个是置信度，后20个是类别概率
+    :param conf_thresh: cls-specific confidence score的阈值
+    :param iou_thresh: NMS算法中iou的阈值
+    """
+    n = bbox.size()[0]
+    bbox_prob = bbox[:, 5:].clone()  # 类别预测的条件概率
+    bbox_confi = bbox[:, 4].clone().unsqueeze(1).expand_as(bbox_prob)  # 预测置信度
+    bbox_cls_spec_conf = bbox_confi * bbox_prob  # 置信度*类别条件概率=cls-specific confidence score整合了是否有物体及是什么物体的两种信息
+    bbox_cls_spec_conf[bbox_cls_spec_conf <= conf_thresh] = 0  # 将低于阈值的bbox忽略
+    for c in range(20):
+        rank = torch.sort(bbox_cls_spec_conf[:, c], descending=True).indices
+        for i in range(98):
+            if bbox_cls_spec_conf[rank[i], c] != 0:
+                for j in range(i + 1, 98):
+                    if bbox_cls_spec_conf[rank[j], c] != 0:
+                        iou = calculate_iou(bbox[rank[i], 0:4], bbox[rank[j], 0:4])
+                        if iou > iou_thresh:  # 根据iou进行非极大值抑制抑制
+                            bbox_cls_spec_conf[rank[j], c] = 0
+    bbox = bbox[torch.max(bbox_cls_spec_conf, dim=1).values > 0]  # 将20个类别中最大的cls-specific confidence score为0的bbox都排除
+    bbox_cls_spec_conf = bbox_cls_spec_conf[torch.max(bbox_cls_spec_conf, dim=1).values > 0]
+    res = torch.ones((bbox.size()[0], 6))
+    res[:, 1:5] = bbox[:, 0:4]  # 储存最后的bbox坐标信息
+    res[:, 0] = torch.argmax(bbox[:, 5:], dim=1).int()  # 储存bbox对应的类别信息
+    res[:, 5] = torch.max(bbox_cls_spec_conf, dim=1).values  # 储存bbox对应的class-specific confidence scores
+    return res
 
 
 # 注意检查一下输入数据的格式，到底是xywh还是xyxy
@@ -126,21 +146,36 @@ class YOLOv1ControlPanel(wx.Panel):
         self.spendTimeTXT.Enable(False)
         hhbox.Add(self.spendTimeTXT, 0, wx.LEFT | wx.TOP, 3)
         vbox.Add(hhbox)
+        vbox.Add((-1, 5))
+        hhbox = wx.BoxSizer()
+        hhbox.Add((10, -1))
+        self.enablePicBTN = wx.Button(self, label="图片", size=(-1, 30))
+        hhbox.Add(self.enablePicBTN, 1, wx.RIGHT, 5)
+        self.enableLabelBTN = wx.Button(self, label="标签", size=(-1, 30))
+        hhbox.Add(self.enableLabelBTN, 1, wx.RIGHT, 5)
+        self.enableBoxBTN = wx.Button(self, label="检测结果", size=(-1, 30))
+        hhbox.Add(self.enableBoxBTN, 1, wx.RIGHT, 5)
+        vbox.Add(hhbox, 0, wx.EXPAND)
         vbox.Add((-1, 3))
         vbox.Add(wx.StaticLine(self, style=wx.HORIZONTAL), 0, wx.EXPAND)
         vbox.Add((-1, 3))
         hhbox = wx.BoxSizer()
         hhbox.Add((10, -1))
         if self.currentPosition:
-            self.label = wx.StaticText(self, label="第%d行，第%d列BOX数据：" % (self.currentPosition[0] + 1, self.currentPosition[1] + 1))
+            self.label = wx.StaticText(self, label="第%d行，第%d列BOX数据：" % (
+                self.currentPosition[0] + 1, self.currentPosition[1] + 1))
         else:
             self.label = wx.StaticText(self, label="第 行，第 列BOX数据：")
-        hhbox.Add(self.label,0)
+        hhbox.Add(self.label, 0)
         vbox.Add(hhbox, 0, wx.EXPAND)
         self.resultDisplayPanel = ResultDisplayPanel(self, self.log)
         vbox.Add(self.resultDisplayPanel, 1, wx.EXPAND)
         self.SetSizer(vbox)
-        self.Bind(wx.EVT_BUTTON, self.OnStartDetection)
+        self.startDetectionBTN.Bind(wx.EVT_BUTTON, self.OnStartDetection)
+        self.enablePicBTN.Bind(wx.EVT_BUTTON, self.OnEnablePicBTN)
+
+    def OnEnablePicBTN(self,event):
+        self.parent.middlePanel.enablePicture = not self.parent.middlePanel.enablePicture
 
     def OnStartDetection(self, event):
         modelName = modelsDir + self.pretrainedModelCOMBO.GetValue()
@@ -158,6 +193,8 @@ class YOLOv1ControlPanel(wx.Panel):
         self.pred = np.squeeze(self.pred)
         self.pred = self.pred.permute((1, 2, 0))
         self.bbox = labels2bbox(self.pred)
+        self.parent.middlePanel.bbox = NMS(self.bbox)
+        self.parent.middlePanel.Refresh()
 
 
 # 发现一个问题，使用torch.load()方法，只有在main里能用，在子程序中使用会报错
